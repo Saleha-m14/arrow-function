941a85af6810801a52d6e5f1fc804c4538054e6a
Saleha Mohammadi <saleha.moh2023@gmail.com>
2023-08-27 14:12:21 +0000

    Arrow function in JavaScript

--- arrow-functions.js
+++ arrow-functions.js
@@ -3,12 +3,34 @@
  * command node arrow-functions.js in the terminal
  */
 
-// Vanilla JavaScript Function
+/* Vanilla JavaScript Function
+function addTwoNumber(a, b) {
+    // code block
+    return a + b;
+}
+let sum = addTwoNumber(3, 5);
+console.log(sum);*/
 
-// Arrow Function With Parameters
+//Arrow Function With Parameters
+const addTwoNumber = (a, b) => {
+    return a + b;
+}
+let sum = addTwoNumber(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2); 
 
 // Implicit Returns
-
+const saySomething = message => console.log(message);
+saySomething("Hello there!");
+const sayHello = () => console.log("HI");
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    '<p>
+        This is a multiple line string.
+    </p>'
+)
+console.log(returnMultipleLines);
